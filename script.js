fcf476cf1442dcd79334ba455fde6698e461ec9a
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-04-17 02:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
 function functionName() {
-  alert('functionName');
+  alert(arguments.callee.name);
+}
+
+function AccioJob() {
+  alert(arguments.callee.name');
 }
 functionName();
-function AccioJob() {
-  alert('AccioJob');
-}
 AccioJob();
